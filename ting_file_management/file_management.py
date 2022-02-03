13f18866dad8e0b2272b7e2e4dee16ed70824e47
PedroSincero <dckpedro@gmail.com>
2022-02-03 16:27:06 -0300

    fix - finalizando requisti 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,14 @@
+# ref >>>
+# https://www.geeksforgeeks.org/python-string-splitlines-method/
+# https://www.delftstack.com/howto/python/python-print-to-stderr/
+import sys
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    try:
+        if not path_file.endswith('.txt'):
+            return sys.stderr.write('Formato inválido\n')
+        path = open(path_file)        
+    except FileNotFoundError:
+        return sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
+    with path as file:
+        return file.read().splitlines()
