422ed5616e7e48ebb588e52cf51bbd73816857d0
PedroSincero <dckpedro@gmail.com>
2022-02-03 18:07:06 -0300

    fix - finalizando requisito 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,19 @@
+# from .queue import Queue
+from .file_management import txt_importer
+import sys
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+
+    if path_file not in instance.data:
+        file_txt = txt_importer(path_file)
+        len_file = len(file_txt)
+        instance.enqueue(path_file)
+        data = {
+            'nome_do_arquivo': path_file,
+            'qtd_linhas': len_file,
+            'linhas_do_arquivo': file_txt
+        }
+        return sys.stdout.write(str(data))
 
 
 def remove(instance):
