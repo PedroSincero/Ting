00da340a69dfd3d113cdf092905e3ded44952413
PedroSincero <dckpedro@gmail.com>
2022-02-03 18:54:57 -0300

    fix - finalizando requisito 5

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -6,15 +6,15 @@ import sys
 def process(path_file, instance):
 
     if path_file not in instance.data:
-        file_txt = txt_importer(path_file)
-        len_file = len(file_txt)
         instance.enqueue(path_file)
-        data = {
-            'nome_do_arquivo': path_file,
-            'qtd_linhas': len_file,
-            'linhas_do_arquivo': file_txt
-        }
-        return sys.stdout.write(str(data))
+    file_txt = txt_importer(path_file)
+    len_file = len(file_txt)
+    data = {
+        'nome_do_arquivo': path_file,
+        'qtd_linhas': len_file,
+        'linhas_do_arquivo': file_txt
+    }
+    return sys.stdout.write(str(data))
 
 
 def remove(instance):
@@ -26,4 +26,9 @@ def remove(instance):
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        path_file = instance.search(position)
+        result = process(path_file, instance)
+        return sys.stderr.write(str(result))
+    except IndexError:
+        sys.stderr.write('Posição inválida')
