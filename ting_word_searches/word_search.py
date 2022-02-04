12bf79b487d934db6b9c0f6e21237b0ba62b59bc
PedroSincero <dckpedro@gmail.com>
2022-02-04 16:26:03 -0300

    fix - finalizando requisito 6

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,17 +1,35 @@
 def exists_word(word, instance):
    result = []
-   search_inst = instance.search(0)
-   print(' o ser supremo',search_inst)
-   return search_inst
-   # return instance.search(0)
-   # process(word, instance.data[0])
-   # result = [{
-   #    "palavra": word,
-   #    "arquivo": instance.data[0],
-   #    "ocorrencias": valor
-   # }]
-
+   for data in instance.data:
+      currences = []
+      for lines in  data['linhas_do_arquivo']:
+         if not lines.count(word):
+            return result
+         currences.append({'linha': lines.count(word)})
+      result.append({
+         "palavra": word,
+         "arquivo": data['nome_do_arquivo'],
+         "ocorrencias": currences,
+      })
+   return result
 
 
 def search_by_word(word, instance):
     """Aqui irá sua implementação"""
+
+a = 'oiiooiioio'
+if a.count('z'):
+   print(a.count('z'))
+
+#  {
+#   'linhas_do_arquivo': ['Aqui contem um texto que fala sobre um menino pobre '
+#                         'chamado Pedro.'],
+#   'nome_do_arquivo': 'statics/nome_pedro.txt',
+#   'qtd_linhas': 1,
+#  }
+
+   #  {
+   #      "palavra": "Pedro",
+   #      "arquivo": "statics/nome_pedro.txt",
+   #      "ocorrencias": [{"linha": 1}],
+   #  }
