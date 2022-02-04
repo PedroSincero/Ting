d3926185a48db46b57125c5a90d2642e3e3566aa
PedroSincero <dckpedro@gmail.com>
2022-02-04 16:00:43 -0300

    fix - finalizando alteraçoes no requisito 5

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -5,8 +5,9 @@ import sys
 
 def process(path_file, instance):
 
-    if path_file not in instance.data:
-        instance.enqueue(path_file)
+    for index in range(len(instance.data)):
+        if path_file == instance.search(index)['nome_do_arquivo']:
+            return None
     file_txt = txt_importer(path_file)
     len_file = len(file_txt)
     data = {
@@ -14,6 +15,7 @@ def process(path_file, instance):
         'qtd_linhas': len_file,
         'linhas_do_arquivo': file_txt
     }
+    instance.enqueue(data)
     return sys.stdout.write(str(data))
 
 
@@ -21,14 +23,17 @@ def remove(instance):
     if not len(instance.data):
         return sys.stdout.write('Não há elementos\n')
 
-    sys.stdout.write(f"Arquivo {instance.data[0]} removido com sucesso\n")
-    instance.dequeue()
+    remove_file = instance.dequeue()
+    sys.stdout.write(f"Arquivo {remove_file['nome_do_arquivo']} removido com sucesso\n")
 
 
 def file_metadata(instance, position):
     try:
         path_file = instance.search(position)
-        result = process(path_file, instance)
-        return sys.stderr.write(str(result))
     except IndexError:
         sys.stderr.write('Posição inválida')
+    else:
+        sys.stdout.write(str(path_file))
+
+# Agradecimentos a Alessandra Rezende - Turma 10 - Tribo B 
+# Pelo Auxilio na Refatoração do Codigo ^^
--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,16 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+   result = []
+   search_inst = instance.search(0)
+   print(' o ser supremo',search_inst)
+   return search_inst
+   # return instance.search(0)
+   # process(word, instance.data[0])
+   # result = [{
+   #    "palavra": word,
+   #    "arquivo": instance.data[0],
+   #    "ocorrencias": valor
+   # }]
+
 
 
 def search_by_word(word, instance):
